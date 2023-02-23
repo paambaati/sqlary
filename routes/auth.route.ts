f009cad1061ea85edee9a24447b1df0879628618
GP <exchequer598@gmail.com>
2023-02-23 14:08:56 +0530

    fix(auth): gracefully handle timingSafeEqual range errors

--- routes/auth.route.ts
+++ routes/auth.route.ts
@@ -16,22 +16,47 @@ interface APIKeyRoute extends RouteGenericInterface {
 const getApiKeyHandler: RouteHandler<APIKeyRoute> = (request, reply) => {
 	const payload = request.body;
 	const { username, password } = payload;
-	if (Users.has(username) && timingSafeEqual(Buffer.from(Users.get(username) || ''), Buffer.from(password))) {
-		if (AuthKeys.has(username)) {
-			return reply.code(200).send({
-				username,
-				apiKey: AuthKeys.get(username),
-			});
-		}
-		return reply.code(404).send({
+
+	const return401 = () =>
+		reply.code(401).send({
+			username,
+			error: 'Credentials provided were incorrect; please try again!',
+		});
+
+	const return404 = () =>
+		reply.code(404).send({
 			username,
 			error: 'No API key found for user; please generate one before proceeding!',
 		});
+
+	if (Users.has(username)) {
+		const lookedUpPassword = Users.get(username) as string;
+		let passwordMatched = false;
+		try {
+			passwordMatched = timingSafeEqual(Buffer.from(lookedUpPassword), Buffer.from(password));
+		} catch (err) {
+			// NOTE: When password lengths do not match, `timingSafeEqual()` throws a `RangeError`,
+			// as it expects both buffers to be of the same length. If we happen to catch any other
+			// error, we log it just in case.
+			/* istanbul ignore next */
+			if (err instanceof Error && err.name !== 'RangeError') {
+				request.log.warn(err, 'Timing-safe password equality check failed!');
+			}
+		}
+		if (passwordMatched) {
+			if (AuthKeys.has(username)) {
+				return reply.code(200).send({
+					username,
+					apiKey: AuthKeys.get(username),
+				});
+			} else {
+				return return404();
+			}
+		} else {
+			return return401();
+		}
 	}
-	return reply.code(401).send({
-		username,
-		error: 'Credentials provided were incorrect; please try again!',
-	});
+	return return401();
 };
 
 export const getAPIKeyRoute: RouteOptions<Server, IncomingMessage, ServerResponse, APIKeyRoute> = {
